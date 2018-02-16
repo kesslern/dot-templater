89e1a221413f10e5a882fdfe05eb1caf8bf9b3f6
Nathan Kessler <nathant93@gmail.com>
2018-02-15 23:00:00 -0500

    Improve config parsing with '=' chars

--- config.c
+++ config.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -7,52 +8,27 @@
 
 #define IS_COMMENT(x) (*(x) == '#')
 
-void substitution_parser(char *str, config *config)
-{
-    // TODO: Handle case where the value is all '=' characters
-    static char *current_key = NULL;
-    static char *current_value = NULL;
-
-    if (str != NULL) {
-        if (current_key == NULL) {
-            current_key = str;
-        } else {
-            if (current_value == NULL) {
-                current_value = str;
-            } else {
-                *(str - 1) = '=';
-            }
-        }
-    } else {
-        (*config->substitution_saver)(current_key, current_value);
-        current_key = NULL;
-        current_value = NULL;
-    }
-}
-
-void tokenizer(char *str, char *delims, void (*handler)(char *, config *),
-               config *config)
-{
-    char *saveptr;
-    char *pos;
-
-    pos = strtok_r(str, delims, &saveptr);
-    (*handler)(pos, config);
-
-    do {
-        pos = strtok_r(NULL, delims, &saveptr);
-        (*handler)(pos, config);
-    } while (pos != NULL);
-}
-
 void parse_line(char *str, config *config)
 {
     if (str != NULL && !IS_COMMENT(str)) {
-        tokenizer(str, "=", &substitution_parser, config);
+        char *split = strchr(str, '=');
+        if (split != NULL) {
+            *split = '\0';
+            (*config->substitution_saver)(str, ++split);
+        }
     }
 }
 
 void parse_configuration(char *str, config config)
 {
-    tokenizer(str, "\n", &parse_line, &config);
+    char *saveptr;
+    char *pos;
+
+    pos = strtok_r(str, "\n", &saveptr);
+    parse_line(pos, &config);
+
+    do {
+        pos = strtok_r(NULL, "\n", &saveptr);
+        parse_line(pos, &config);
+    } while (pos != NULL);
 }
