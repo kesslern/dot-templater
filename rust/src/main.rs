87267e1673ba6f9b454b81394e4e6b5f081753ac
Nathan Kessler <nathant93@gmail.com>
2019-06-05 22:38:16 -0400

    Add command line arguments

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,11 +6,28 @@ use dot_templater::trim_trailing_slash;
 use std::error::Error;
 use std::fs::File;
 use std::fs;
+use std::env;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::process;
 use walkdir::WalkDir;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 4 {
+        eprintln!("Wrong arguments: expected 4 found {}", args.len()-1);
+        process::exit(1);
+    }
+
+    let rules_file = &args[1];
+    let copy_from = &args[2];
+    let copy_to = &args[3];
+
+    println!("Rules: {}", rules_file);
+    println!("From: {}", copy_from);
+    println!("To: {}", copy_to);
+    
     let file = BufReader::new(File::open("mew")?);
     let config = get_config(file.lines());
  
@@ -22,9 +39,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     for feature in config.features {
         println!("Found a feature: {}", feature);
     }
-
-    let copy_from = "../test";
-    let copy_to = "imaginaryFolder/../bar/";
 
     let copy_from = trim_trailing_slash(copy_from);
     let copy_to = trim_trailing_slash(copy_to);
