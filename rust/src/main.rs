586009627615f6dd0bb103cc9cb23fac250c9147
Nathan Kessler <nathant93@gmail.com>
2019-06-05 20:28:59 -0400

    Walk directories and get some metadata

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,18 @@
 extern crate dot_templater;
+extern crate walkdir;
 
 use dot_templater::get_config;
 use std::error::Error;
 use std::fs::File;
+use std::fs;
 use std::io::BufRead;
 use std::io::BufReader;
+use walkdir::WalkDir;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let file = BufReader::new(File::open("mew")?);
     let config = get_config(file.lines());
-    
+ 
     for (key, value) in config.substitutions {
         println!("Found key: {}", key);
         println!("Found value: {}", value);
@@ -17,6 +20,20 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     for feature in config.features {
         println!("Found a feature: {}", feature);
+    }
+
+    let copy_from = "../test";
+    let copy_to = "imaginaryFolder/../bar/";
+
+    for entry in WalkDir::new(copy_from) {
+        let entry = entry?;
+        let path = entry.path();
+        let dest = path.to_str().unwrap().replace(copy_from, copy_to);
+        println!("Source: {}", path.display());
+        println!("Dest: {}", dest);
+        let attr = fs::metadata(path)?;
+        println!("Is directory: {}", attr.is_dir());
+        println!("-----------------");
     }
 
     Ok(())
