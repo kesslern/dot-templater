1fd42960150b9ca804eca771a02c988dc3d6b93f
Nathan Kessler <nathant93@gmail.com>
2020-10-08 21:50:36 -0400

    Add release script

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$1
+
+if [ -z "$VERSION" ]
+then
+    echo "No release version provided."
+    exit 1
+fi
+
+echo Creating release for dot-templater v$VERSION
+
+mkdir dot-templater
+cargo build --release
+cp target/release/dot-templater dot-templater/
+tar czf dot-templater-$VERSION-linux-x64.tar.gz dot-templater
+rm -rf dot-templater/
